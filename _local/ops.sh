77e257fb30e52cc36684c52affe2de5ac8cf6ce3
mirage335 <spamfreemirage335@gmail.com>
2022-02-05 17:37:35 -0500

    Documentation.

---
+++ _local/ops.sh
@@ -0,0 +1,8 @@
+#Enable search if "vm.img" and related files are missing.
+export ubVirtImageLocal="false"
+
+export ub_anchor_autoupgrade="true"
+
+#export ub_anchor_specificSoftwareName="experimental"
+#export ub_anchor_user="true"
+
