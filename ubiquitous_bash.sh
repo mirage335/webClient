71d99e76d142215ba3d45a7c40a69cfd70ea9eb4
mirage335 <spamfreemirage335@gmail.com>
2022-02-24 10:18:10 -0500

    Upstream.

--- ubiquitous_bash.sh
+++ ubiquitous_bash.sh
@@ -32,7 +32,7 @@ _ub_cksum_special_derivativeScripts_contents() {
 #export ub_setScriptChecksum_disable='true'
 ( [[ -e "$0".nck ]] || [[ "${BASH_SOURCE[0]}" != "${0}" ]] || [[ "$1" == '--profile' ]] || [[ "$1" == '--script' ]] || [[ "$1" == '--call' ]] || [[ "$1" == '--return' ]] || [[ "$1" == '--devenv' ]] || [[ "$1" == '--shell' ]] || [[ "$1" == '--bypass' ]] || [[ "$1" == '--parent' ]] || [[ "$1" == '--embed' ]] || [[ "$1" == '--compressed' ]] || [[ "$0" == "/bin/bash" ]] || [[ "$0" == "-bash" ]] || [[ "$0" == "/usr/bin/bash" ]] || [[ "$0" == "bash" ]] ) && export ub_setScriptChecksum_disable='true'
 export ub_setScriptChecksum_header='1891409836'
-export ub_setScriptChecksum_contents='3193190969'
+export ub_setScriptChecksum_contents='2090027340'
 
 # CAUTION: Symlinks may cause problems. Disable this test for such cases if necessary.
 # WARNING: Performance may be crucial here.
@@ -5167,6 +5167,31 @@ _stopwatch() {
 	measureDateB=$(date +%s%N | cut -b1-13)
 
 	bc <<< "$measureDateB - $measureDateA"
+}
+
+
+_test_python() {
+	_getDep python
+}
+
+
+
+_test_haskell() {
+	if [[ -e /etc/issue ]] && cat /etc/issue | grep 'Debian' > /dev/null 2>&1
+	then
+		_wantGetDep '/usr/share/doc/haskell-platform/README.Debian'
+	fi
+
+	_wantGetDep alex
+	_wantGetDep cabal
+	_wantGetDep happy
+	_wantGetDep HsColour
+	_wantGetDep hscolour
+
+	_wantGetDep ghc
+	_wantGetDep ghci
+	! type -p 'ghc' && echo 'warn: missing: ghc'
+	! type -p 'ghci' && echo 'warn: missing: ghci'
 }
 
 
@@ -17659,6 +17684,16 @@ _test() {
 	
 	_getDep yes
 	
+	
+	
+	_getDep perl
+	
+	
+	_tryExec "_test_python"
+	_tryExec "_test_haskell"
+	
+	
+	
 	_test_readlink_f
 	
 	_tryExec "_test_package"
@@ -19016,14 +19051,7 @@ class ubPythonPS1(object):
 sys.ps1 = ubPythonPS1()
 sys.ps2 = "\x01\033[0;96m\x02|...\x01\033[0m\x02 "
 
-
-
-
 #_python()
-
-
-
-
 
 CZXWXcRMTo8EmM8i4d
 }
@@ -19032,6 +19060,9 @@ CZXWXcRMTo8EmM8i4d
 _generate_lean-overrides-python_here() {
 	cat << 'CZXWXcRMTo8EmM8i4d'
 
+# WARNING: Strongly discouraged example.
+# (strongly prefer to inherit a single os.environ['scriptAbsoluteFolder'] environment variable from being called by an 'ubiquitous_bash' script)
+#exec(open(_getScriptAbsoluteFolder()+'/lean.py').read())
 
 
 
@@ -19040,37 +19071,21 @@ _generate_lean-overrides-python_here() {
 
 
 
+
+
+
+# ATTENTION: NOTICE: Environment variables from 'ubiquitous_bash' can be used to import other python scripts.
+#exec(open(os.environ['scriptAbsoluteFolder']+'/lean.py').read())
 
 #################################################
-# ATTENTION: Add '_prog' script code here!
-
-
-
-
-
-
+# ATTENTION: NOTICE: Add '_prog' script code here!
 
 def _main():
 	pass
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ATTENTION: Add '_prog' script code here!
+# ATTENTION: NOTICE: Add '_prog' script code here!
 #################################################
 
 
@@ -19080,9 +19095,10 @@ if sys.hexversion > 0x03000000:
 
 import sys
 import string
-#./lean.py "_python(c('1 + 2'))"
+#./lean.py "_python(c('1 + 2'))" #FAIL
+#python3 ./lean.py "_print(c('1 + 2'))"
+#python2 ./lean.py "print(c('1 + 2'))"
 #./lean.py "_print(c('1 + 2'))"
-#./lean.py "print(c('1 + 2'))"
 # https://www.tutorialspoint.com/python/python_command_line_arguments.htm
 # https://www.programiz.com/python-programming/methods/built-in/exec
 # https://www.geeksforgeeks.org/python-program-to-convert-a-list-to-string/
@@ -19096,18 +19112,7 @@ if len(sys.argv) > 1:
 		exec( sys.argv[1] )
 
 
-
-
-
-
-
-
-
-
-
 _main()
-
-
 
 CZXWXcRMTo8EmM8i4d
 }
